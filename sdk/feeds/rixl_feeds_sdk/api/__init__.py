00bf8006e32c93cee451e67654a789fc04f659e2
Yusuf Akinleye <ayaaakinleye@gmail.com>
2026-03-23 11:24:55 +0100

    feat: add sdk #86ewyy49g

---
+++ sdk/feeds/rixl_feeds_sdk/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from rixl_feeds_sdk.api.feeds_api import FeedsApi
+
---
+++ sdk/videos/rixl_videos_sdk/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from rixl_videos_sdk.api.videos_api import VideosApi
+
